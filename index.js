6c44b232745d5ed828f524628b47954a4892b693
Brajesh Kumar <73804577+brajesh623@users.noreply.github.com>
2022-09-22 20:11:05 +0530

    Git commit live data updated in statitistics section

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-  GitHubCalendar(".calendar", "brajesh623", { responsive: true });
+GitHubCalendar(".calendar", "brajesh623", { responsive: true });
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+sleep(100).then(() => {
+  let x=document.querySelector(".contrib-number").innerText;
+  x=x.split(" ");
+  x=(+(x[0])--);
+  x=x-(x%10);
+
+
+  document.querySelector("#contrib-number").innerText="";
+  document.querySelector("#contrib-number").innerText=x+"+";
+})
